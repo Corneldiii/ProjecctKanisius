bee967eaf59fa692125e0bae3b43a662b1984671
Your Name <you@example.com>
2024-10-28 20:26:08 +0700

    insert memo

--- application/views/memo.php
+++ application/views/memo.php
@@ -18,7 +18,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 style="display: flex; justify-content: flex-start; margin-top: 20px; margin-bottom: 20px; border-bottom: 2px solid #ddd;">
                 <a href="menuKeluar" class="btn btn-primary active"
                     style="display: inline-block; padding: 12px 20px; margin: 0 10px; font-size: 16px; text-decoration: none; color: #555; border: 1px solid #ddd; border-bottom: none; background-color: #f9f9f9; border-radius: 8px 8px 0 0; transition: background-color 0.3s ease, color 0.3s ease;">Tabel</a>
-                <a href="insertKeluar" class="btn btn-primary"
+                <a href="inputMemo" class="btn btn-primary"
                     style="display: inline-block; padding: 12px 20px; margin: 0 10px; font-size: 16px; text-decoration: none; color: #555; border: 1px solid #ddd; border-bottom: none; background-color: #f9f9f9; border-radius: 8px 8px 0 0; transition: background-color 0.3s ease, color 0.3s ease;">Insert</a>
             </div>
 
