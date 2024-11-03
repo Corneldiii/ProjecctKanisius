513f9c9c42d2146d95ddd016929a3c72f7b55737
StefanusDickyBayuSegara <bayusegara1011@gmail.com>
2024-11-03 18:14:03 +0700

    memperbaiki header

--- application/views/header.php
+++ application/views/header.php
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,13 +10,17 @@
 
     <title>Aplikasi Surat</title>
     <link rel="icon" href="img/icon.png">
-
+    
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
     <link href="css/sb-admin-2.css" rel="stylesheet">
     <link rel="stylesheet" href="css/modal-login.css">
-    <link rel="stylesheet" href="css/spiner.css">
-
+    <link rel="stylesheet"  href="css/spiner.css">
+    
+    <!-- CSS jika diperlukan -->
     <style>
         textarea {
             resize: none;
@@ -36,7 +41,7 @@
 
             <a class="sidebar-brand d-flex align-items-center justify-content-center">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-mail-bulk"></i>
+                    <i class="fas fa-mail-bulk"></i> <!-- cari ikon di: https://fontawesome.com/v5/search?q=laptop-medical&o=r -->
                 </div>
                 <div class="sidebar-brand-text mx-3">SURAT</div>
             </a>
@@ -47,7 +52,7 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUser"
                    aria-expanded="true" aria-controls="collapseUser" id="menuUser">
                     <i class="fas fa-fw fa-user"></i>
-                    <span>Login sebagai USER</span>
+                    <span>Login sebagai USER</span> <!-- USER = nomor pegawai -->
                 </a>
                 <div id="collapseUser" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
@@ -124,8 +129,6 @@
                         <a class="collapse-item" href="insert" style="white-space: normal;">Laporan - laporan</a>
                     </div>
                 </div>
+
             </li>
-        </ul>
-    </div>
-</body>
-</html>
+        </ul>
